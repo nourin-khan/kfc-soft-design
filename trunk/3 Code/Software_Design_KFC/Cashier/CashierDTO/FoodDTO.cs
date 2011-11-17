614cd55a70151ea791aa9287a29546e8102f33e8
hathao298@gmail.com <hathao298@gmail.com@04257774-9648-dfa0-a1d0-eb8f01ce2138>
2011-11-17 16:09:37 +0000

    Add properties for DTO classes Editor: JML

--- trunk/3 Code/Software_Design_KFC/Cashier/CashierDTO/FoodDTO.cs
+++ trunk/3 Code/Software_Design_KFC/Cashier/CashierDTO/FoodDTO.cs
@@ -7,5 +7,17 @@ namespace CashierDTO
 {
     public class FoodDTO
     {
+        #region Attributes - private
+        private string _foodID;
+        private string _foodName;
+        private float _foodPrice;
+        private float _discountPrice;
+        private string _image;
+        private string _description;
+        private string _foodGroupID;
+        #endregion
+
+        #region Attributes - public
+        #endregion
     }
 }
--- trunk/3 Code/Software_Design_KFC/Kitchen/KitchenDTO/OrderDTO.cs
+++ trunk/3 Code/Software_Design_KFC/Kitchen/KitchenDTO/OrderDTO.cs
@@ -7,5 +7,21 @@ namespace KitchenDTO
 {
     public class OrderDTO
     {
+        /* @ convention:
+         * _orderStatus: 
+         * 0 - deleted
+         * 1 - not confirm yet
+         * 2 - already confirm
+         */
+        #region Attributes - private
+        private string _orderID;
+        private DateTime _orderDate;
+        private int _tableNum;
+        private int _orderStatus;
+        private string _orderNote;
+        #endregion
+
+        #region Attributes - public
+        #endregion
     }
 }
