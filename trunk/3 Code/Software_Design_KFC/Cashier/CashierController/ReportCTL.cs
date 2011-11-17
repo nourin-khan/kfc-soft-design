08df0f5e5f2c1748a48022f0ad060f878b985987
hathao298@gmail.com <hathao298@gmail.com@04257774-9648-dfa0-a1d0-eb8f01ce2138>
2011-11-17 11:43:18 +0000

    Rebuild (restructure) the project Editor: JML

---
+++ trunk/3 Code/Software_Design_KFC/Cashier/CashierController/ReportCTL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CashierController
+{
+    public class ReportCTL
+    {
+    }
+
+    public class DailyReport : ReportCTL
+    {
+    }
+
+    public class WeeklyReport : ReportCTL
+    {
+    }
+
+    public class MonthlyReport : ReportCTL
+    {
+    }
+}
---
+++ trunk/3 Code/Software_Design_KFC/Cashier/CashierDTO/ReportDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CashierDTO
+{
+    public class ReportDTO
+    {
+    }
+
+    public class DailyReport : ReportDTO
+    {
+    }
+
+    public class WeeklyReport : ReportDTO
+    {
+    }
+
+    public class MonthlyReport : ReportDTO
+    {
+    }
+}
