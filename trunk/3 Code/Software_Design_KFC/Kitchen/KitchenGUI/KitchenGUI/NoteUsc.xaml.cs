7d0a41f56a375b586a27238c3113df533f24a4dd
hathao298@gmail.com <hathao298@gmail.com@04257774-9648-dfa0-a1d0-eb8f01ce2138>
2011-12-06 13:23:29 +0000

    Upload KitchenGUI version1 Author: JML

---
+++ trunk/3 Code/Software_Design_KFC/Kitchen/KitchenGUI/KitchenGUI/NoteUsc.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace KitchenGUI
+{
+	/// <summary>
+	/// Interaction logic for OrderGUI.xaml
+	/// </summary>
+	public partial class NoteUsc : UserControl
+    {
+        #region Attribute
+        public string noteId = "123";
+        #endregion
+        public NoteUsc()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
