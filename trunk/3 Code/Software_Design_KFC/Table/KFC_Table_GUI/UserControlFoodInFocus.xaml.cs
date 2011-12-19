e8731ed3d03c4e45df6491a6cb2a3a8c6dc6a295
vantuanlee@gmail.com <vantuanlee@gmail.com@04257774-9648-dfa0-a1d0-eb8f01ce2138>
2011-12-19 07:10:43 +0000

    handle  food in focus when user click on food control repair handle for some buttons : add to cart and close in food control

---
+++ trunk/3 Code/Software_Design_KFC/Table/KFC_Table_GUI/UserControlFoodInFocus.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KFC_Table_GUI
+{
+	/// <summary>
+	/// Interaction logic for UserControlFoodInFocus.xaml
+	/// </summary>
+	public partial class UserControlFoodInFocus : Window
+	{
+        private bool isAddToCart = false;
+
+        public bool IsAddToCart
+        {
+            get { return isAddToCart; }
+            set { isAddToCart = value; }
+        }
+
+        public string Details 
+        {
+            get {return details.Text;}
+            set {details.Text = value;}
+        }
+        
+        public Image Image
+        {
+            get {return image;}
+            set {image = value;}
+        }
+
+		public UserControlFoodInFocus()
+		{
+			this.InitializeComponent();
+            // set window startup position
+            this.WindowStartupLocation = WindowStartupLocation.Manual;
+            this.Left = 350;
+            this.Top = 300;
+		}
+
+		private void close_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+            this.Close();
+		}
+
+		private void addToCart_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+            this.isAddToCart = true;
+            this.Close();
+		}
+	}
+}
