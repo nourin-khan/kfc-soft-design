7869b321c5de80a33b43c4984e2b90b0faf8d2b4
hathao298@gmail.com <hathao298@gmail.com@04257774-9648-dfa0-a1d0-eb8f01ce2138>
2011-12-27 16:48:39 +0000

    Upload CashierGui version 1

---
+++ trunk/3 Code/Software_Design_KFC/Cashier/CashierGUI/TableUsc.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CashierGUI
+{
+	/// <summary>
+	/// Interaction logic for TableUsc.xaml
+	/// </summary>
+	public partial class TableUsc : UserControl
+	{
+		private bool _free = true;
+		
+		public bool free
+		{
+			get {return _free;}
+			set 
+			{
+                _free = value;
+                if (value == false)
+                {
+                    this.freeLbl.Visibility = Visibility.Hidden;
+                }
+                else
+                {
+                    this.freeLbl.Visibility = Visibility.Visible;
+                }
+			}
+		}
+		public TableUsc()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
