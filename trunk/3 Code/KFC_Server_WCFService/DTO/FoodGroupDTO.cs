049dc2462ddf1751459aa52d228831f044259ccf
vantuanlee@gmail.com <vantuanlee@gmail.com@04257774-9648-dfa0-a1d0-eb8f01ce2138>
2011-12-28 07:53:58 +0000

    move server from KFC_Server solution to KFC_Server_WCFService solution complete all DAO classes

---
+++ trunk/3 Code/KFC_Server_WCFService/DTO/FoodGroupDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    public class FoodGroupDTO
+    {
+        #region Attributes - private
+        private string _foodGroupID;
+
+        public string FoodGroupID
+        {
+            get { return _foodGroupID; }
+            set { _foodGroupID = value; }
+        }
+        private string _foodGroupName;
+       
+        public string FoodGroupName
+        {
+            get { return _foodGroupName; }
+            set { _foodGroupName = value; }
+        }
+        #endregion
+
+        public FoodGroupDTO(string id, string name)
+        {
+            // TODO: Complete member initialization
+            this.FoodGroupID = id;
+            this.FoodGroupName = name;
+        }
+    }
+    
+}
---
+++ trunk/3 Code/KFC_Server_WCFService/ServiceLibrary/DAO/ReportDAO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ServiceLibrary
+{
+    public class ReportDAO
+    {
+        public class ReportDTO
+        {
+        }
+
+        public class DailyReport : ReportDTO
+        {
+        }
+
+        public class WeeklyReport : ReportDTO
+        {
+        }
+
+        public class MonthlyReport : ReportDTO
+        {
+        }
+    }
+}
